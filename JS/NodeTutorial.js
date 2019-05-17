f08a22367c71a4c24cfc7cb425a82fd2cf621e16
Kitsueki <kitsueki17@gmail.com>
2019-05-17 00:27:35 -0400

    Initial playground

---
+++ JS/NodeTutorial.js
@@ -0,0 +1,11 @@
+var Tutor=require('./Tutorial.js');
+exports.NodeTutorial=function()
+{
+	console.log("Node Tutorial")
+}
+
+exports.pTutor = function()
+{
+	var PTutor=Tutor
+	PTutor.tutorial();
+}
